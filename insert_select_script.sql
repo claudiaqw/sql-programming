dd2e1d47d327c8d8d18805053225043d0357f4ae
Claudia Quintana Wong <claudiaqw94@gmail.com>
2020-12-03 17:59:34 +0100

    certificate, country, studio poblated

--- insert_select_script.sql
+++ insert_select_script.sql
@@ -1,19 +1,32 @@
 use FilmStaging;
 
 SET IDENTITY_INSERT Actor ON;
-
 INSERT INTO Actor(ActorId, ActorName)
 SELECT DISTINCT ActorId, ActorName  
 FROM FilmStaging;
-
 SET IDENTITY_INSERT Actor OFF;
 
-
 SET IDENTITY_INSERT Director ON;
-
 INSERT INTO Director(DirectorId,DirectorName)
 SELECT DISTINCT DirectorId, DirectorName
 FROM FilmStaging
-
 SET IDENTITY_INSERT Director OFF;
 
+SET IDENTITY_INSERT [Certificate] ON; 
+INSERT INTO [Certificate](CertificateId, [Certificate])
+SELECT DISTINCT CertificateId, [Certificate]
+FROM FilmStaging
+WHERE CertificateId IS NOT NULL
+SET IDENTITY_INSERT [Certificate] OFF;
+
+SET IDENTITY_INSERT Country ON;
+INSERT INTO Country(CountryId, CountryName)
+SELECT DISTINCT CountryId, CountryName
+FROM FilmStaging
+SET IDENTITY_INSERT Country OFF;
+
+SET IDENTITY_INSERT Studio ON;
+INSERT INTO Studio(StudioId, StudioName)
+SELECT DISTINCT FilmStudioId, StudioName
+FROM FilmStaging
+SET IDENTITY_INSERT Studio OFF;
